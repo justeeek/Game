9be8d48f98de3b727ec74a632e4495217f4469de
justeeek <101112066+justeeek@users.noreply.github.com>
2023-03-03 23:30:22 +0400

    Add files via upload

---
+++ DAL/HotlineContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DAL
+{
+    public partial class HotlineContext : DbContext
+    {
+        public HotlineContext()
+        {
+        }
+
+        public HotlineContext(DbContextOptions<HotlineContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Game> Games { get; set; }
+        public virtual DbSet<Player> Players { get; set; }
+        public virtual DbSet<Session> Sessions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host = localhost; Database = Hotline; Username = postgres; Password = 12345678;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>(entity =>
+            {
+                entity.ToTable("Game");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.HostId).HasColumnName("HostID");
+
+                entity.HasOne(d => d.Host)
+                    .WithMany(p => p.Games)
+                    .HasForeignKey(d => d.HostId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("HostID");
+            });
+
+            modelBuilder.Entity<Player>(entity =>
+            {
+                entity.ToTable("Player");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnType("character varying");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnType("character varying");
+            });
+
+            modelBuilder.Entity<Session>(entity =>
+            {
+                entity.ToTable("Session");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .UseIdentityAlwaysColumn();
+
+                
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Sessions)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("PlayerID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
